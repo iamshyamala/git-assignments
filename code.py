318da64e5c9f71fab758be4fec4521780530a148
iamshyamala <devopslife99@gmail.com>
2022-11-22 02:29:13 +0530

    first commit

---
+++ code.py
@@ -0,0 +1,7 @@
+pi = 3.142                                   # I could have also used "math" library (math.pi)
+radius = 5                                   # Interger value for radius
+area_of_circle = 0                           # Used to store the value of area of circle
+
+area_of_circle = pi * (radius) ** 2          # Area = (PI * R^2)
+
+print("The area of the circle based on the given data is: ", area_of_circle)
